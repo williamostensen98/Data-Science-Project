5063207ab17eb98496fb7bb7ac679bed64f71804
William Østensen <william-98-@hotmail.com>
2022-05-22 15:47:50 +0200

    added missing class TriplestoreQueryProcessor

--- project/impl.py
+++ project/impl.py
@@ -95,7 +95,175 @@ class QueryProcessor(ABC):
         pass
 
 
-class RelationalQueryProcessor(QueryProcessor):
+class RelationalQueryProcessor(QueryProcessor, RelationalProcessor):
 
+    # TODO: Implement relational getPublicationsPublishedInYear
     def getPublicationsPublishedInYear(self, year: int) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getPublicationsByAuthorId
+    def getPublicationsByAuthorId(self, id: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getMostCitedPublication
+    def getMostCitedPublication(self) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getMostCitedVenue
+    def getMostCitedVenue(self) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getVenuesByPublisherId
+    def getVenuesByPublisherId(self, id: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getPublicationInVenue
+    def getPublicationInVenue(self, venueId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getJournalArticlesInIssue
+    def getJournalArticlesInIssue(self, issue: str, volume: str, journalId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getJournalArticlesInVolume
+    def getJournalArticlesInVolume(self, volume: str, journalId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getJournalArticlesInJournal
+    def getJournalArticlesInJournal(self, journalId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getProceedingsByEvent
+    def getProceedingsByEvent(self, eventPartialName: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getPublicationAuthors
+    def getPublicationAuthors(self, publicationId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getPublicationByAuthorName
+    def getPublicationByAuthorName(self, authorPartialName: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement relational getDistinctPublishersOfPublications
+    def getDistinctPublishersOfPublications(self, pubIdList: list(str)) -> pd.DataFrame:
+        pass
+
+
+class TripletoreQueryProcessor(QueryProcessor, TriplestoreProcessor):
+
+    # TODO: Implement triplestore getPublicationsPublishedInYear
+    def getPublicationsPublishedInYear(self, year: int) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getPublicationsByAuthorId
+    def getPublicationsByAuthorId(self, id: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getMostCitedPublication
+    def getMostCitedPublication(self) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getMostCitedVenue
+    def getMostCitedVenue(self) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getVenuesByPublisherId
+    def getVenuesByPublisherId(self, id: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getPublicationInVenue
+    def getPublicationInVenue(self, venueId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getJournalArticlesInIssue
+    def getJournalArticlesInIssue(self, issue: str, volume: str, journalId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getJournalArticlesInVolume
+    def getJournalArticlesInVolume(self, volume: str, journalId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getJournalArticlesInJournal
+    def getJournalArticlesInJournal(self, journalId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getProceedingsByEvent
+    def getProceedingsByEvent(self, eventPartialName: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getPublicationAuthors
+    def getPublicationAuthors(self, publicationId: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getPublicationByAuthorName
+    def getPublicationByAuthorName(self, authorPartialName: str) -> pd.DataFrame:
+        pass
+
+    # TODO: Implement triplestore getDistinctPublishersOfPublications
+    def getDistinctPublishersOfPublications(self, pubIdList: list(str)) -> pd.DataFrame:
+        pass
+
+
+class GenericQueryProcessor(QueryProcessor):
+
+    def __init__(self) -> None:
+        self.queryProcessor = None
+
+    def cleanQueryProcessors(self) -> None:
+        pass
+
+    def addQueryProcessor(self, processor: QueryProcessor) -> None:
+        pass
+
+    # TODO: Implement generic getPublicationsPublishedInYear
+    def getPublicationsPublishedInYear(self, year: int):
+        pass
+
+    # TODO: Implement generic getPublicationsByAuthorId
+    def getPublicationsByAuthorId(self, id: str):
+        pass
+
+    # TODO: Implement generic getMostCitedPublication
+    def getMostCitedPublication(self):
+        pass
+
+    # TODO: Implement generic getMostCitedVenue
+    def getMostCitedVenue(self):
+        pass
+
+    # TODO: Implement generic getVenuesByPublisherId
+    def getVenuesByPublisherId(self, id: str):
+        pass
+
+    # TODO: Implement generic getPublicationInVenue
+    def getPublicationInVenue(self, venueId: str):
+        pass
+
+    # TODO: Implement generic getJournalArticlesInIssue
+    def getJournalArticlesInIssue(self, issue: str, volume: str, journalId: str):
+        pass
+
+    # TODO: Implement generic getJournalArticlesInVolume
+    def getJournalArticlesInVolume(self, volume: str, journalId: str):
+        pass
+
+    # TODO: Implement generic getJournalArticlesInJournal
+    def getJournalArticlesInJournal(self, journalId: str):
+        pass
+
+    # TODO: Implement generic getProceedingsByEvent
+    def getProceedingsByEvent(self, eventPartialName: str):
+        pass
+
+    # TODO: Implement generic getPublicationAuthors
+    def getPublicationAuthors(self, publicationId: str):
+        pass
+
+    # TODO: Implement generic getPublicationByAuthorName
+    def getPublicationByAuthorName(self, authorPartialName: str):
+        pass
+
+    # TODO: Implement generic getDistinctPublishersOfPublications
+    def getDistinctPublishersOfPublications(self, pubIdList: list(str)):
         pass
